de3ef67bd21096f84d5b3608909abe2b3d8364c6
주기현 <0318joo@naver.com>
2025-01-02 13:46:32 +0900

    프로젝트 생성

---
+++ src/main/java/rlguswn/JavaShop/JavaShopApplication.java
@@ -0,0 +1,13 @@
+package rlguswn.JavaShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rlguswn/JavaShop/JavaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package rlguswn.JavaShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
