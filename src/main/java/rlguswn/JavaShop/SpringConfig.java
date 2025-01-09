1a9844a660354c9b50ae02ebf19fc5a189c33553
주기현 <0318joo@naver.com>
2025-01-09 20:12:14 +0900

    #2 service bean으로 등록

--- src/main/java/rlguswn/JavaShop/SpringConfig.java
+++ src/main/java/rlguswn/JavaShop/SpringConfig.java
@@ -2,14 +2,25 @@ package rlguswn.JavaShop;
 
 import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import rlguswn.JavaShop.Security.CustomUserDetailService;
 import rlguswn.JavaShop.repository.*;
+import rlguswn.JavaShop.service.*;
 
+@Configuration
 public class SpringConfig {
 
     private final EntityManager em;
 
     public SpringConfig(EntityManager em) {
         this.em = em;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
@@ -45,5 +56,45 @@ public class SpringConfig {
     @Bean
     public OrderItemRepository orderItemRepository() {
         return new JpaOrderItemRepository(em);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(CustomUserDetailService customUserDetailService) {
+        return customUserDetailService;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository(), passwordEncoder());
+    }
+
+    @Bean
+    public ProductService productService() {
+        return new ProductService(productRepository());
+    }
+
+    @Bean
+    public ProductImageService productImageService() {
+        return new ProductImageService(productImageRepository());
+    }
+
+    @Bean
+    public CartService cartService() {
+        return new CartService(cartRepository());
+    }
+
+    @Bean
+    public CartItemService cartItemService() {
+        return new CartItemService(cartItemRepository());
+    }
+
+    @Bean
+    public CustomerOrderService customerOrderService() {
+        return new CustomerOrderService(customerOrderRepository());
+    }
+
+    @Bean
+    public OrderItemService orderItemService() {
+        return new OrderItemService(orderItemRepository());
     }
 }
