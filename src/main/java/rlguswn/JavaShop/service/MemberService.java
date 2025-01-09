d1542d90f3a0e48225b211a4486e14af66d783e2
주기현 <0318joo@naver.com>
2025-01-09 17:17:49 +0900

    #2 memberService 구현

---
+++ src/main/java/rlguswn/JavaShop/service/MemberService.java
@@ -0,0 +1,65 @@
+package rlguswn.JavaShop.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import rlguswn.JavaShop.domain.Member;
+import rlguswn.JavaShop.enums.Role;
+import rlguswn.JavaShop.repository.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class MemberService {
+
+    @Autowired
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public MemberService(MemberRepository memberRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Member signUp(String email, String password, String username, String address) {
+        // email 중복 확인 필요
+
+        String encodedPassword = passwordEncoder.encode(password);
+
+        Member member = new Member(
+                email,
+                encodedPassword,
+                username,
+                address,
+                Role.MEMBER
+        );
+        return memberRepository.save(member);
+    }
+
+    public Optional<Member> findById(Long id) {
+        return memberRepository.findById(id);
+    }
+
+    public Optional<Member> findByEmail(String email) {
+        return memberRepository.findByEmail(email);
+    }
+
+    public List<Member> findAll() {
+        return memberRepository.findAll();
+    }
+
+    public Member getLoginMember() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = ((UserDetails) authentication.getPrincipal()).getUsername();
+        return memberRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("멤버를 찾을 수 없습니다."));
+    }
+}
