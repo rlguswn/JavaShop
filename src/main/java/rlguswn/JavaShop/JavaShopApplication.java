a198e3d3c77a66b8c36557fa9680fa722ce9a603
주기현 <0318joo@naver.com>
2025-01-16 16:08:49 +0900

    #2 jpa repository 지정

--- src/main/java/rlguswn/JavaShop/JavaShopApplication.java
+++ src/main/java/rlguswn/JavaShop/JavaShopApplication.java
@@ -2,8 +2,10 @@ package rlguswn.JavaShop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "rlguswn.JavaShop.repository")
 public class JavaShopApplication {
 
 	public static void main(String[] args) {
